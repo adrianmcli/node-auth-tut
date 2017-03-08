5b92c80a2dd77c7b6466b2e413b39bb2d12041f6
Adrian Li <li.adrianmc@gmail.com>
2017-03-07 20:38:06 -0800

    implement database connection and methods

--- index.js
+++ index.js
@@ -4,6 +4,48 @@ const port = process.env.PORT || 8080
 
 app.set('view engine', 'ejs')
 
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise  // use native promises
+const dbConfig = {
+  url: 'mongodb://127.0.0.1:27017/node-auth-tut',
+}
+
+const bcrypt = require('bcryptjs')
+const userSchema = mongoose.Schema({
+  local: {
+    email: String,
+    password: String,
+  },
+})
+
+// attach hashgen and password validator methods
+userSchema.methods.generateHash = password =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+
+userSchema.methods.validPassword = password =>
+  bcrypt.compareSync(password, this.local.password)
+
+// expose the model for usage
+const User = mongoose.model('User', userSchema)
+
+// connect to the DB
+mongoose.connect(dbConfig.url)
+
+// EXAMPLE:
+// const testUser = new User({
+//   local: {
+//     email: 'hi@example.com',
+//     password: 'mysecret',
+//   },
+// })
+
+// testUser.save((err) => {
+//   if (err) throw err
+//   console.log('testUser saved successfully!')
+// })
+
+
+
 // TODO - routes will be in its own file later
 // require('./app/routes.js')(app)
 
